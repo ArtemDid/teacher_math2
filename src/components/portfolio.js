09fd9e481c69acd9ef8393b278b6f8f3c9fc3d64
ArtemDid <artem.didenkojs@gmail.com>
2022-02-02 11:40:01 +0200

    change size awards and portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -14,8 +14,18 @@ export const App = () => {
                     <div class="p-2 bd-highlight" style={{ fontSize: 48, color: 'blue' }}>Моє портфоліо</div>
                 </div>
                 <br />
-                <div style={{ textAlign: 'center' }}>
+                <div class="d-flex flex-column align-items-center">
                     <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRMKtvF6PgmFXJyPphbpCsfX50CHZjeKAYkMEBTecSMw8rzKGbxdbcflSzdZElmHQ/embed?start=true&loop=false&delayms=3000" frameborder="0" width="1180" height="900" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    <br /><br /><br />
+                    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRWQZEGYoA-PcSF3lXfW8jvp_OQqWodoP6X2gtF7YPtaD4ZFfMceqRnNFFVCx-PPg/embed?start=true&loop=false&delayms=3000" frameborder="0" width="1180" height="900" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    <br /><br /><br />
+                    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRYi-hPTPgDXsU3mScEIH_VwZHONdoJI3R7TioGoUV-TdgVjLdOHeY_wU1mAGr0fg/embed?start=true&loop=false&delayms=3000" frameborder="0" width="1180" height="900" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    <br /><br /><br />
+                    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRL5Ta3CJ0FIuOTkmNp1eMpnwvL5NDI9LfjDsidt9tuSqF2w5T5BK4J_j0SyHE_JQ/embed?start=true&loop=false&delayms=3000" frameborder="0" width="1180" height="900" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    <br /><br /><br />
+                    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vTEj2vTtwlwfY0OOxBNnRVMjHSe-tSsEpNAfTzIsvn5psv2qLq1oDDqi4l07gCeKg/embed?start=true&loop=false&delayms=3000" frameborder="0" width="1180" height="900" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                    <br /><br /><br />
+                    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vSWJO6A-eAUT0XjkEX7_nw1HJYDDY_0Qv8dkpelBXuCeQ-LCyVgbig5NHRvFdObHw/embed?start=true&loop=false&delayms=3000" frameborder="0" width="1180" height="900" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
                 </div>
             </Content>
         </Layout>
